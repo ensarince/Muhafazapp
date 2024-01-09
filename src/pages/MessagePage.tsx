cba3fac601714f83645ac52c1924842b1795176c
Ensar <ensar.ince@didactic-innovations.de>
2024-01-09 22:15:42 +0100

    minor styling

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -30,7 +30,7 @@ export default function MessagePage({ }: Props) {
         </Button>
 
         <div className={styles.container}>
-          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:"100%", height:"7.5rem", color:"#fff", background:colors.secondary_blue_softer}}>
+          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:{xs:"100%", sm:"100%", md:"50%", lg:"30%", xl:"30%"}, height:"7.5rem", color:"#fff", background:colors.secondary_blue_softer}}>
             <img style={{borderRadius:"100%", width:"50px", height:"50px"}} src="https://media.licdn.com/dms/image/D4E03AQHrfiYzSpQtfA/profile-displayphoto-shrink_800_800/0/1670598092910?e=2147483647&v=beta&t=ldSIoQ5_ckvbA4B0KfpO1avskwWnuUYaC1P96kaWtew" alt="" />
             <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"start"}}>
               <img style={{width:"2rem"}} src="https://media.partydeko.de/media/catalog/product/cache/8/image/9df78eab33525d08d6e5fb8d27136e95/h/a/hawaiihemd-sunset-on-the-beach-1146_1.jpg" alt="" />
@@ -42,7 +42,7 @@ export default function MessagePage({ }: Props) {
               </Typography>
             </Box>
           </Box>
-          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:"100%", height:"7.5rem", color:"#fff", background:colors.secondary_blue_soft}}>
+          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:{xs:"100%", sm:"100%", md:"50%", lg:"30%", xl:"30%"}, height:"7.5rem", color:"#fff", background:colors.secondary_blue_soft}}>
             <img style={{borderRadius:"100%", width:"50px", height:"50px"}} src="https://media.licdn.com/dms/image/D5603AQHoizKyKrgIdg/profile-displayphoto-shrink_800_800/0/1698086293267?e=1710374400&v=beta&t=nLmC-gvLuqil3QaB202noQG8XA9RD6EOe_YTrFIZqBc" alt="" />
             <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"start"}}>
               <img style={{width:"2rem"}} src="https://i.etsystatic.com/34187105/r/il/42f625/4277147086/il_570xN.4277147086_3tfb.jpg" alt="" />
@@ -54,7 +54,7 @@ export default function MessagePage({ }: Props) {
               </Typography>
             </Box>
           </Box>
-          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:"100%", height:"7.5rem", color:"#fff", background:colors.secondary_blue}}>
+          <Box sx={{display:"grid", gridTemplateColumns:"5rem 14rem", justifyContent:"center", alignItems:"center", width:{xs:"100%", sm:"100%", md:"50%", lg:"30%", xl:"30%"}, height:"7.5rem", color:"#fff", background:colors.secondary_blue}}>
             <img style={{borderRadius:"100%", width:"50px", height:"50px"}} src="https://media.licdn.com/dms/image/C5103AQH28HY5DiAkDA/profile-displayphoto-shrink_400_400/0/1552307079505?e=2147483647&v=beta&t=QLetHtxKGpEz9bbPbjes5KfIAJwSVBHkc_MZnphFff4" alt="" />
             <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"start"}}>
               <img style={{width:"2rem"}} src="https://cdn.test.de/file/image/ct/produktbilder/302020183001/v1/u1322-3/hauptbild_original/hauptbild_original;w835.webp" alt="" />
